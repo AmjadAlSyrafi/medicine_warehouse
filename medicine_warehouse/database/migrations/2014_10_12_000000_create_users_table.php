d6efc82efd20809cbf81bd58c2c882f1480f7651
AmjadAlSyrafi <mhdamjadalsyrafi@gmail.com>
2023-11-14 02:36:11 +0200

    correct the dataBase

--- medicine_warehouse/database/migrations/2014_10_12_000000_create_users_table.php
+++ medicine_warehouse/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_number')->nullable();
+            $table->string('role')->default('pharmacy'); // pharmacy or admin
             $table->rememberToken();
             $table->timestamps();
         });
